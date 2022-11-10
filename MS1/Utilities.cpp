463f7db9bae8525313285b94f564c38e70762834
Rahul Bajaj <rahulbajaj0242@gmail.com>
2022-11-10 09:34:34 -0500

    Initial attempt, not checked!

---
+++ MS1/Utilities.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "Utilities.h"
+#include <string.h>
+#include "sstream"
+
+using namespace std;
+
+namespace sdds {
+
+  std::string Utilities::trim(std::string &str){
+    int start = str.find_first_not_of(' ');
+    int end = str.find_last_not_of(' ');
+    return str.substr(start, end-start+1);
+  }
+
+  void Utilities::setFieldWidth(size_t newWidth) {
+    m_widthField = newWidth;
+  }
+
+  size_t Utilities::getFieldWidth() const {
+    return m_widthField;
+  }
+
+  std::string Utilities::extractToken(const std::string& str, size_t& next_pos, bool& more) {
+    string token;
+    string stringAtPos = str.substr(next_pos);
+    stringstream s(stringAtPos);
+    if(stringAtPos[0] == m_delimiter) {
+      throw "Delimiter Found!";
+    }
+    if(getline(s, token, m_delimiter)) {
+      token = trim(token);
+      size_t currPos = str.find(token);
+      next_pos = str.find(m_delimiter, currPos) + 1;
+      more = true;
+      return token;
+    }
+    else {
+      more = false;
+    }
+  }
+
+  void Utilities::setDelimiter(char newDelimiter) {
+    m_delimiter = newDelimiter;
+  }
+
+  char Utilities::getDelimiter() {
+    return m_delimiter;
+  }
+
+}
