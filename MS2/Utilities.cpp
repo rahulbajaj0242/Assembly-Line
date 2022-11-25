4dc176c4c113af09809089583b3dfe6126dd8f97
Rahul Bajaj <rahulbajaj0242@gmail.com>
2022-11-25 09:27:41 -0500

    deleted comments in Utilities in ms2

--- MS2/Utilities.cpp
+++ MS2/Utilities.cpp
@@ -1,3 +1,11 @@
+// Name: Rahul Bajaj
+// Seneca Student ID: 107707218
+// Seneca email: rbajaj12@myseneca.ca
+// Date of completion: Nov 16, 2022
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
 #include <iostream>
 #include "Utilities.h"
 #include <string.h>
@@ -43,16 +51,16 @@ namespace sdds {
         return token;
       }
       else if(getline(s, token, m_delimiter)) {
-        // size_t currPos = next_pos + token.length() + 1;
-        next_pos = next_pos+token.length() + 1;
+        size_t currPos = str.find(token);
         token = trim(token);
+        next_pos = str.find(m_delimiter, currPos);
         more = true;
-        // if(next_pos == string::npos) {
-        //   more= false;
-        // }
-        // else {
-        //   next_pos++;
-        // }
+        if(next_pos == string::npos) {
+          more= false;
+        }
+        else {
+          next_pos++;
+        }
         if(m_widthField < token.size()) m_widthField= token.size();
         return token;
       }
@@ -60,43 +68,6 @@ namespace sdds {
     }
     return "NULL";
   }
-
-  // std::string Utilities::extractToken(const std::string& str, size_t& next_pos, bool& more)
-	// {
-	// 	std::string token{};
-	// 	std::size_t position = str.find(getDelimiter(), next_pos);
-
-	// 	if (position == next_pos)
-	// 	{
-	// 		more = false;
-	// 		throw std::string("Not found!");
-	// 	}
-	// 	else if (position != std::string::npos)
-	// 	{
-	// 		token = str.substr(next_pos, position - next_pos);
-	// 		next_pos = token.find_first_not_of(" ");
-	// 		token.erase(token.begin(), token.begin() + next_pos);
-	// 		size_t end = token.find_last_not_of(" ");
-	// 		token.erase(end + 1);
-	// 		m_widthField = std::max(m_widthField, token.length());
-	// 		next_pos = position + 1;
-	// 		more = true;
-	// 	}
-	// 	else if (position == std::string::npos)
-	// 	{
-	// 		token = str.substr(next_pos, position - next_pos);
-	// 		next_pos = token.find_first_not_of(" ");
-	// 		token.erase(token.begin(), token.begin() + next_pos);
-	// 		size_t end = token.find_last_not_of(" ");
-	// 		token.erase(end + 1);
-	// 		m_widthField = std::max(m_widthField, token.length());
-	// 		next_pos = position + 1;
-	// 		more = false;
-	// 	}
-
-	// 	return token;
-
-	// }
 
   void Utilities::setDelimiter(char newDelimiter) {
     m_delimiter = newDelimiter;
